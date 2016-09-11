8cdc5864c3eeabc1483cba1f6752d48ca6d662c9
ArchaeaTenebris <sometimes98@bk.ru>
2016-09-11 17:46:30 +0600

    v 0.0
    Added starting functional, support for Intellij IDEA

---
+++ Quantis/src/Launcher.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Андрей on 11.09.2016.
+ */
+public class Launcher {
+    public static void main ( String [] args) {
+        System.out.println("Quantis launched. Please type in  the command.");
+    }
+}
---
+++ Quantis/src/Quantis.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Андрей on 11.09.2016.
+ */
+public class Quantis {
+
+}
